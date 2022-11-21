7e7606b91bde90bac14e179f7458eb47df6e44c2
khanh181 <91577673+khanh181@users.noreply.github.com>
2022-11-21 09:37:29 +0700

    course detail page vs [CURD] create new course

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
+var slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
 
 const Schema = mongoose.Schema;
 
-const Course = new Schema({
-    name: { type: String, minlength: 1 },
-    description: { type: String },
-    image: { type: String },
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
-});
+const Course = new Schema(
+    {
+        name: { type: String, minlength: 1, required: true },
+        description: { type: String },
+        image: { type: String },
+        videoID: { type: String },
+        slug: { type: String, slug: 'name', unique: true },
+    },
+    {
+        timestamps: true,
+    },
+);
 
 module.exports = mongoose.model('Course', Course);
